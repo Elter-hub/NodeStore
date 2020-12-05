462a5bfafa8f760354873be10585a4c1b1085bc0
ihor <ihor04@gmail.com>
2020-12-05 23:39:00 +0200

    stripe works fine

--- services/payment/buyProducts.service.js
+++ services/payment/buyProducts.service.js
@@ -3,32 +3,26 @@ const User = require('../../models/User.model');
 // eslint-disable-next-line import/order
 const stripe = require('stripe')(STRIPE_SECRET_KEY);
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     try {
         const {
             email, token, sum, userId
         } = req.body;
-        stripe.customers.create({
+        const customer = await stripe.customers.create({
             email,
             source: token,
             name: 'Ihor D',
-        })
-            .then((customer) => stripe.charges.create({
-                amount: sum * 100,
-                description: 'Learning Node js',
-                currency: 'UAH',
-                customer: customer.id
-            }))
-            .then(async () => {
-                const user = await User.findOne({ email });
-                user.cart.products = [];
-                user.save();
-                res.json({ message: 'Success' });
-            })
-            .catch((err) => {
-                console.log(err);
-                res.send(err);
-            });
+        });
+        await stripe.charges.create({
+            amount: sum * 100,
+            description: 'Learning Node js',
+            currency: 'UAH',
+            customer: customer.id
+        });
+        const user = await User.findOne({ email });
+        user.cart.products = [];
+        user.save();
+        res.json({ message: 'Success' });
     } catch (error) {
         console.log(error);
         res.status(400).json({ message: error });
